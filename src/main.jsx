faf13b92f187ac30d415a1e0c9c7d2536507f422
JordyV23 <jordyvargas0123@gmail.com>
2023-04-29 21:38:59 -0600

    Uso de varios custom hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,20 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
-// import { SimpleForm } from "./02-useEffect/SimpleForm";
+import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+//import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
+//import { SimpleForm } from "./02-useEffect/SimpleForm";
 //import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 //import { CounterApp } from "./01-useState/CounterApp";
 //import {HooksApp} from './HooksApp'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    {/* <HooksApp/> */}
-    {/* <CounterApp/> */}
-    {/* <CounterWithCustomHook /> */}
-    {/* <SimpleForm /> */}
 
-    <FormWithCustomHook/>
-  </React.StrictMode>
+    // {/* <HooksApp/> */}
+    // {/* <CounterApp/> */}
+    // {/* <CounterWithCustomHook /> */}
+    // {/* <SimpleForm /> */}
+    // {/* <FormWithCustomHook/> */}
+    <MultipleCustomHooks/>
+
 );
